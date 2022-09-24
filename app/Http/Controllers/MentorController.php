f0d719152a3e5520cc96bb660201aac6ba9df527
Richieid23 <fitrarizki23@gmail.com>
2022-09-25 06:47:43 +0700

    add mentor API - update

--- app/Http/Controllers/MentorController.php
+++ app/Http/Controllers/MentorController.php
@@ -93,7 +93,39 @@ class MentorController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'name' => 'required|string',
+            'profile' => 'required|url',
+            'email' => 'required|email',
+            'profession' => 'required|string'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return \response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::find($id);
+        if (!$mentor) {
+            return \response()->json([
+                'status' => 'error',
+                'message' => 'mentor not found'
+            ], 404);
+        }
+
+        $mentor->fill($data);
+        $mentor->save();
+
+        return \response()->json([
+            'status' => 'success',
+            'data' => $mentor
+        ]);
     }
 
     /**

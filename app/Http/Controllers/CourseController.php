ca707b8d42f6f9f3c1ffe6f5a2ae5fba2ece91d8
Richieid23 <fitrarizki23@gmail.com>
2022-09-26 11:35:46 +0700

    add courses API - list & delete

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -14,9 +14,25 @@ class CourseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $courses = Course::query();
+
+        $q = $request->query('q');
+        $status = $request->query('status');
+
+        $courses->when($q, function ($query) use ($q) {
+            return $query->whereRaw("name LIKE '%" . \strtolower($q) . "%'");
+        });
+
+        $courses->when($status, function ($query) use ($status) {
+            return $query->where("status", $status);
+        });
+
+        return \response()->json([
+            'status' => 'success',
+            'data' => $courses->paginate(10)
+        ]);
     }
 
     /**
@@ -166,6 +182,20 @@ class CourseController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $course = Course::find($id);
+
+        if (!$course) {
+            return \response()->json([
+                'status' => 'error',
+                'message' => 'course not found'
+            ], 404);
+        }
+
+        $course->delete();
+
+        return \response()->json([
+            'status' => 'success',
+            'message' => 'course deleted successfully'
+        ]);
     }
 }
